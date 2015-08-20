fff8bd65f67b696fe5d3b5703b72b6339ef29129
Christian Höltje <docwhat@gerf.org>
2015-08-20 13:49:13 -0400

    style fixes

--- lib/list_to_columns.rb
+++ lib/list_to_columns.rb
@@ -1,5 +1,6 @@
-require "list_to_columns/version"
+require 'list_to_columns/version'
 
+# Allow Arrays to be displayed as columnar lists instead.
 module ListToColumns
   # Your code goes here...
 end
